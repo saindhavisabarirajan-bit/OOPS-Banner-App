ab64d52551d020a61668f828e83ddd131b69f273
saindhavisabarirajan-bit <saindhavisabarirajan@gmail.com>
2026-02-24 10:14:22 +0530

    refactor: use String.join for memory efficiency in UC3

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,16 @@
 /**
  * OOPSBannerApp
- * UC2: Stylized Asterisk Banner
+ * UC3: Refactoring banner using String.join() for better memory management.
  * @author YourName
- * @version 2.0
+ * @version 3.0
  */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("  *** *** **** **** ");
-        System.out.println(" * * * * * * * ");
-        System.out.println(" * * * * **** *** ");
-        System.out.println(" * * * * * *");
-        System.out.println("  *** *** * **** ");
+        // Constructing each line using String.join for efficiency
+        System.out.println(String.join("", "  *** ", " *** ", "**** ", "**** "));
+        System.out.println(String.join("", " * * ", "* * ", "P   * ", "S    "));
+        System.out.println(String.join("", " * * ", "* * ", "**** ", " *** "));
+        System.out.println(String.join("", " * * ", "* * ", "P     ", "    *"));
+        System.out.println(String.join("", "  *** ", " *** ", "P     ", "**** "));
     }
 }
