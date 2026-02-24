4bfd8b9525671a99f670211f402cf7d047dbb05a
saindhavisabarirajan-bit <saindhavisabarirajan@gmail.com>
2026-02-24 09:45:07 +0530

    feat: implement UC1 - print OOPS literal

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+/**
+ * OOPSBannerApp
+ * UC1: Printing "OOPS" literal to console.
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
